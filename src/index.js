7aa208441ced9249da4c51c52dd2cfd5c1c51eb5
Panther1986 <polzovatel77@gmail.com>
2024-05-16 16:02:16 +0300

    update2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ const PORT = 3002;
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello world',
+    message: 'Hello world, again!',
   });
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
