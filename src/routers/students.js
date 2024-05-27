d473e140803bbb5d9fc518e4af6feb258da21c2e
Panther1986 <polzovatel77@gmail.com>
2024-05-27 22:31:42 +0300

    update8

--- src/routers/students.js
+++ src/routers/students.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   getStudentsController,
   getStudentByIdController,
+  creatrStedetnController,
 } from '../controllers/students.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -10,5 +11,7 @@ const router = Router();
 router.get('/students', ctrlWrapper(getStudentsController));
 
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
+
+router.post('/students', ctrlWrapper(creatrStedetnController));
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -11,7 +11,12 @@ const PORT = Number(env('PORT', '3002'));
 export const startServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
   app.use(cors());
 
   app.use(
