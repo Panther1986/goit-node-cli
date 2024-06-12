dcebf378d278c75846a5bd9a76a222a437484f2c
Panther1986 <polzovatel77@gmail.com>
2024-06-12 23:53:41 +0300

    update14

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { registerUserShema } from '../validation/auth.js';
-import { registerUsersController } from '../controllers/auth.js';
+import { loginUserShema, registerUserShema } from '../validation/auth.js';
+import {
+  loginUserController,
+  registerUsersController,
+} from '../controllers/auth.js';
 
 const router = Router();
 
@@ -10,6 +13,11 @@ router.post(
   '/register',
   validateBody(registerUserShema),
   ctrlWrapper(registerUsersController),
+);
+router.post(
+  '/login',
+  validateBody(loginUserShema),
+  ctrlWrapper(loginUserController),
 );
 
 export default router;
